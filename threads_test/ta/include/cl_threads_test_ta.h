c5940cde03e17ce1445b5f3b02618e258c097a59
nice944 <67176595+nice944@users.noreply.github.com>
2020-07-18 17:18:00 +0800

    Create cl_threads_test_ta.h

---
+++ threads_test/ta/include/cl_threads_test_ta.h
@@ -0,0 +1,16 @@
+#ifndef TA_CL_THREADS_TEST_H
+#define TA_CL_THREADS_TEST_H
+
+
+/*
+ * This UUID is generated with uuidgen
+ * the ITU-T UUID generator at http://www.itu.int/ITU-T/asn1/uuid.html
+ */
+#define TA_CL_THREADS_TEST_UUID \
+	{ 0x9cccf200, 0x2450, 0x11e4, \
+		{ 0xab, 0xe2, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
+
+/* The function IDs implemented in this TA */
+#define TA_CL_THREADS_TEST_CMD_RUN_TEST		0
+
+#endif /*TA_CL_THREADS_TEST_H*/
