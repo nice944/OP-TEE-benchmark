07108683c0bc4da8ff1a709b2499e0b44f86ea1b
nice944 <67176595+nice944@users.noreply.github.com>
2020-07-18 17:24:42 +0800

    Create cl_memory_test_ta.h

---
+++ memory_test/ta/include/cl_memory_test_ta.h
@@ -0,0 +1,21 @@
+#ifndef TA_CL_MEMORY_TEST_H
+#define TA_CL_MEMORY_TEST_H
+
+
+/*
+ * This UUID is generated with uuidgen
+ * the ITU-T UUID generator at http://www.itu.int/ITU-T/asn1/uuid.html
+ */
+#define TA_CL_MEMORY_TEST_UUID \
+	{ 0x9bbbf200, 0x2450, 0x11e4, \
+		{ 0xab, 0xe2, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
+
+/* The function IDs implemented in this TA */
+#define TA_CL_MEMORY_TEST_CMD_SEQ_WRITE		0
+#define TA_CL_MEMORY_TEST_CMD_SEQ_READ		1
+#define TA_CL_MEMORY_TEST_CMD_RND_WRITE		2
+#define TA_CL_MEMORY_TEST_CMD_RND_READ		3
+
+#endif /*TA_CL_MEMORY_TEST_H*/
+
+
